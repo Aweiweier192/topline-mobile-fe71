85ea9941c9862d70dc1b8c348cb5aaf56d2fd7f7
lipengzhou <lpzmail@163.com>
2019-07-11 15:22:19 +0800

    首页文章列表-处理频道数据

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -89,6 +89,14 @@ export default {
         }
       }
 
+      // 修改 channels，将这个数据结构修改为满足我们使用的需求
+      channels.forEach(item => {
+        item.articles = [] // 用来存储当前文章的列表
+        item.downPullLoading = false // 控制当前频道的下拉刷新 loading 状态
+        item.upPullLoading = false // 控制当前频道的上拉加载更多的 loading 状态
+        item.upPullFinished = false // 控制当前频道数据是否加载完毕
+      })
+
       this.channels = channels
     },
 

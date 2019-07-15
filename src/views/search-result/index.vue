b081c05f4d0c53fcafb04912b9934f9fac316546
lipengzhou <lpzmail@163.com>
2019-07-15 16:31:38 +0800

    搜索结果-手动销毁组件禁用缓存

--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -49,6 +49,17 @@ export default {
     }
   },
 
+  activated () {
+    console.log('activated')
+  },
+
+  deactivated () {
+    // console.log('deactivated')
+
+    // 手动销毁当前实例，禁用缓存！！！
+    this.$destroy()
+  },
+
   methods: {
     async onLoad () {
       await this.$sleep(800)

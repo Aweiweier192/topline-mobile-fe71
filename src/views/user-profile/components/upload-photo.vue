a3f595f65811bdb71188c6969eead308ee1b7405
lipengzhou <lpzmail@163.com>
2019-07-20 16:22:48 +0800

    图片选择预览成功

--- src/views/user-profile/components/upload-photo.vue
+++ src/views/user-profile/components/upload-photo.vue
@@ -7,7 +7,12 @@
     >
       <van-cell-group>
         <van-cell title="从相册选择" @click="handleShowFileSelect" />
-        <input style="display: none;" ref="file" type="file">
+        <input
+          style="display: none;"
+          ref="file"
+          type="file"
+          @change="handleFileChange"
+        >
         <van-cell title="拍照" />
         <van-cell title="取消" @click="$emit('input', false)" />
       </van-cell-group>
@@ -16,6 +21,8 @@
 </template>
 
 <script>
+import { ImagePreview } from 'vant'
+
 export default {
   name: 'UploadPhoto',
   props: {
@@ -24,14 +31,41 @@ export default {
       default: false
     }
   },
+
+  data () {
+    return {}
+  },
+
   computed: {
     file () {
       return this.$refs['file']
     }
   },
+
   methods: {
     handleShowFileSelect () {
       this.file.click()
+    },
+
+    handleFileChange () {
+      const reader = new FileReader()
+      reader.readAsDataURL(this.file.files[0])
+      reader.onload = () => {
+        ImagePreview({
+          images: [
+            reader.result // base64 编码的图片文件
+          ],
+          showIndex: false // 是否显示页码
+        })
+      }
+
+      // ImagePreview({
+      //   images: [
+      //     // 这种方式有个问题。不建议使用
+      //     // window.URL.createObjectURL(this.file.files[0])
+      //   ],
+      //   showIndex: false // 是否显示页码
+      // })
     }
   }
 }

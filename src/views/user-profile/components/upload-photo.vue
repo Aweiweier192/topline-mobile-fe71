9465e98b29bdc39e687e0f4026743e0d557b4244
lipengzhou <lpzmail@163.com>
2019-07-20 15:22:35 +0800

    编辑用户-上传头像-点击显示文件弹框

--- src/views/user-profile/components/upload-photo.vue
+++ src/views/user-profile/components/upload-photo.vue
@@ -6,7 +6,8 @@
       :show-confirm-button="false"
     >
       <van-cell-group>
-        <van-cell title="从相册选择" />
+        <van-cell title="从相册选择" @click="handleShowFileSelect" />
+        <input style="display: none;" ref="file" type="file">
         <van-cell title="拍照" />
         <van-cell title="取消" @click="$emit('input', false)" />
       </van-cell-group>
@@ -21,6 +22,16 @@ export default {
     value: {
       type: Boolean,
       default: false
+    }
+  },
+  computed: {
+    file () {
+      return this.$refs['file']
+    }
+  },
+  methods: {
+    handleShowFileSelect () {
+      this.file.click()
     }
   }
 }
